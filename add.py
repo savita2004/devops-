917592823e2cd0f92f3b10c83157265e36e34c57
savita2004 <savita.16996@sakec.ac.in>
2024-10-22 11:27:24 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,15 @@
+def main():
+    first = 10
+    second = 20
+ 
+    # Add the two numbers
+    sum_ = first + second
+ 
+    # Print the result
+    print(f"{first} + {second} = {sum_}")
+ 
+if _name_ == "_main_":
+    main()
+
+
+python first.py
